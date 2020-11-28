d5c788b1bfdbde4ada35027b899a13dc9c196ffc
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2020-11-28 21:47:41 +0700

    improve week summary statistic more

--- app/src/main/java/com/example/awesomehabit/statistic/WeekSummaryActivity.java
+++ app/src/main/java/com/example/awesomehabit/statistic/WeekSummaryActivity.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.awesomehabit.R;
+import com.example.awesomehabit.database.AppDatabase;
+import com.example.awesomehabit.database.running.Run;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -26,11 +28,13 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
 
 public class WeekSummaryActivity extends AppCompatActivity {
     ArrayList<Long> listRunDateInMilli = new ArrayList<>();
     ArrayList<Float> listRunDistance = new ArrayList<>();
+    ArrayList<Long> listRunTimeLength= new ArrayList<>();
     int qualifiedRunDay = 0;
 
     ArrayList<Float> listWaterAmount = new ArrayList<>();
@@ -142,7 +146,41 @@ public class WeekSummaryActivity extends AppCompatActivity {
     }
 
     private void getQualifiedRunDay() {
+        getRunData();
+        float runData = 0;
+        long runTime = 0;
+        for (int i = 0; i < listRunDateInMilli.size(); i++) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(listRunDateInMilli.get(i));
+            Calendar calendar1 = Calendar.getInstance();
+            calendar1.setTimeInMillis(listRunDateInMilli.get(i));
+            if (calendar1.get(Calendar.WEEK_OF_YEAR) == calendar.get(Calendar.WEEK_OF_YEAR) && calendar.get(Calendar.YEAR) == calendar1.get(Calendar.YEAR)) {
+                runData += listRunDistance.get(i);
+                runTime += listRunTimeLength.get(i);
+            }
+        }
+        setContextForRunTextview(runData, runTime);
         qualifiedRunDay = 6;
+    }
+
+    private void setContextForRunTextview(float runData, long runTime) {
+        TextView textView1 = findViewById(R.id.textViewRunDataSum);
+        textView1.setText(runData + " km");
+        textView1 = findViewById(R.id.textViewRunTimeSum);
+        long hour = runTime / 1000 / 60 / 60;
+        long minute = runTime / 1000 / 60 % 60;
+        long second = runTime / 1000 % 60;
+        String temp = "";
+        if (hour < 10)
+            temp += "0";
+        temp += hour + ":";
+        if (minute < 10)
+            temp += "0";
+        temp += minute + ":";
+        if (second < 10)
+            temp += "0";
+        temp += String.valueOf(second);
+        textView1.setText(temp);
     }
 
     private void sendRunIntent() {
@@ -171,6 +209,16 @@ public class WeekSummaryActivity extends AppCompatActivity {
             listRunDateInMilli.add(calendar.getTimeInMillis());
 
             listRunDistance.add((float) (new Random().nextInt(10) + 5));
+        }
+    }
+
+    private void getRunData() {
+        List<Run> temp = AppDatabase.getDatabase(getApplicationContext()).runDao().getAllRun();
+
+        for (int i = 0; i < temp.size(); i++) {
+            listRunDateInMilli.add(Long.parseLong(temp.get(i).timeStart));
+            listRunDistance.add((float) temp.get(i).distance);
+            listRunTimeLength.add(temp.get(i).runningTime);
         }
     }
 
