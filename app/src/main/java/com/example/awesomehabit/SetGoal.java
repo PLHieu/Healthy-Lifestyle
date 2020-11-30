528d47445f3ac02d7143c58cb2614ebc763ef7fb
PLHieu <hieuflong@gmail.com>
2020-11-30 19:28:36 +0700

    set goal

--- app/src/main/java/com/example/awesomehabit/SetGoal.java
+++ app/src/main/java/com/example/awesomehabit/SetGoal.java
@@ -1,5 +1,6 @@
 package com.example.awesomehabit;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import com.example.awesomehabit.database.AppDatabase;
 import com.example.awesomehabit.database.Goal;
 import com.example.awesomehabit.database.Habit;
 import com.example.awesomehabit.generated.callback.OnClickListener;
+import com.example.awesomehabit.running.demo;
 
 public class SetGoal extends AppCompatActivity {
 
@@ -26,19 +28,30 @@ public class SetGoal extends AppCompatActivity {
         EditText sleepGoal=findViewById(R.id.sleepGoal);
         Button ok=findViewById(R.id.setGoalOk);
 
-        ok.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+        ok.setOnClickListener(v -> {
+
+            String regex = "[0-9]";
+
+            String run = distanceGoal.getText().toString();
+            String sleep = sleepGoal.getText().toString();
+            String water = WaterGoal.getText().toString();
+
+            if(!run.matches(regex) || !sleep.matches(regex) || !water.matches(regex)){
+                new AlertDialog.Builder(this)
+                        .setMessage("Value Invalid, Please Try Again !")
+                        .setPositiveButton("Yes", null )
+                        .show();
+            }else{
                 Goal runGoal=db.goalDao().getGoal(Habit.TYPE_RUN);
-                runGoal.target=Integer.parseInt(distanceGoal.getText().toString());
+                runGoal.target=Integer.parseInt(run);
                 db.goalDao().update(runGoal);
 
                 Goal sleepGoal_=db.goalDao().getGoal(Habit.TYPE_SLEEP);
-                sleepGoal_.target=Integer.parseInt(sleepGoal.getText().toString());
+                sleepGoal_.target=Integer.parseInt(sleep);
                 db.goalDao().update(sleepGoal_);
 
                 Goal waterGoal=db.goalDao().getGoal(Habit.TYPE_COUNT);
-                waterGoal.target=Integer.parseInt(WaterGoal.getText().toString());
+                waterGoal.target=Integer.parseInt(water);
                 db.goalDao().update(waterGoal);
             }
         });
