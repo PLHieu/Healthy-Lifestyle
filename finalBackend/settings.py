9dd17d5a97b4cbadff7460c9d49f821fbcd33943
PLHieu <hieuflong@gmail.com>
2020-12-27 06:56:23 +0700

    fix static

--- finalBackend/settings.py
+++ finalBackend/settings.py
@@ -132,10 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 
 # tu them vo
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
