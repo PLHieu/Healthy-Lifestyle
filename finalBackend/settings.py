05690abfe2eda683fb1ca0055bdafb9cadb5e113
Hieu <18120377@student.hcmus.edu.vn>
2020-12-29 15:14:39 +0700

    migrate to postgre

--- finalBackend/settings.py
+++ finalBackend/settings.py
@@ -91,8 +91,13 @@ WSGI_APPLICATION = 'finalBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'finaldb',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'myuser',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- myuser/models.py
+++ myuser/models.py
@@ -1,21 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+from .managers import MyUserManager
 
 class MyUser(AbstractUser):
+    
     # Delete not use field
     username = None
-    last_login = None
-    is_staff = None
-    is_superuser = None
-
     password = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = MyUserManager() 
 
     def __str__(self):
         return self.email
-
 
 
 
