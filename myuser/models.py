e44b98d31c874470340ad9f373e3f5abe69c3124
Hieu <18120377@student.hcmus.edu.vn>
2021-01-18 16:57:59 +0700

    add doctor patient

--- myuser/models.py
+++ myuser/models.py
@@ -17,8 +17,22 @@ class MyUser(AbstractUser):
     def __str__(self):
         return self.email
 
+# Create your models here.
+class Doctor(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    benhvien = models.CharField(max_length=100)
+    chuyennganh = models.CharField(max_length = 100)
+    sonamcongtac = models.IntegerField()
+    trinhdo = models.CharField(max_length=50)
+    def __str__(self):
+        return self.user
 
-
-
-
+# Create your models here.
+class Patient(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='%(class)s_bn')
+    tenbenh = models.CharField(max_length = 100)
+    thoigiandieutri = models.IntegerField()
+    bacsiquanly = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='%(class)s_bsql')
+    def __str__(self):
+        return self.user
 
