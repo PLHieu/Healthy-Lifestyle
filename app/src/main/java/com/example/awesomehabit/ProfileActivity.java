7e3765e1d63b74cc56de36576e6c559d717d690a
ĐẠT TRẦN TUẤN <18120313@student.hcmus.edu.vn>
2021-01-19 20:44:52 +0700

    add feature in profile

--- app/src/main/java/com/example/awesomehabit/ProfileActivity.java
+++ app/src/main/java/com/example/awesomehabit/ProfileActivity.java
@@ -4,6 +4,10 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -15,7 +19,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -25,15 +32,47 @@ public class ProfileActivity extends AppCompatActivity{
     final ArrayList<String> temp = new ArrayList<>(Arrays.asList("Male", "Female", "Other"));
     Spinner spinner;
     TextView txtViewBirthday;
+    ImageView imgViewIcon;
+
+    final int RESULT_LOAD_IMG = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         spinner = findViewById(R.id.spinnerUserSex);
         txtViewBirthday = findViewById(R.id.txtViewUserBirthday);
+        imgViewIcon = findViewById(R.id.imgViewUserIcon);
 
         initSpinner();
         txtViewBirthday.setOnClickListener(v -> setActionForTxtCalendar());
+        imgViewIcon.setOnClickListener(v -> setImage());
+    }
+
+    private void setImage() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMG);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (reqCode == RESULT_LOAD_IMG)
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    imgViewIcon.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(ProfileActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+
+            } else {
+                Toast.makeText(ProfileActivity.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
     }
 
     private void setActionForTxtCalendar() {
